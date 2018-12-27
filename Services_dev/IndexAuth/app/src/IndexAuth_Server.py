5cd95c698d75943e672f74b5857ef96e792d79d5
samuelxu999 <samuelxu999@gmail.com>
2018-12-27 18:45:24 -0500

    Add IndexAuth service.

---
+++ Services_dev/IndexAuth/app/src/IndexAuth_Server.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3.5
+
+'''
+========================
+IndexAuth_Server module
+========================
+Created on Dec.27, 2018
+@author: Xu Ronghua
+@Email:  rxu22@binghamton.edu
+@TaskDescription: This module provide encapsulation of Hashed Index Authentication Microservices API that handle and response client's request.
+'''
+
+import datetime
+import json
+from flask import Flask, jsonify
+from flask import abort,make_response,request
+from IndexAuth_Policy import IndexPolicy
+
+app = Flask(__name__)
+
+now = datetime.datetime.now()
+datestr=now.strftime("%Y-%m-%d")
+timestr=now.strftime("%H:%M:%S")
+
+#========================================== Error handler ===============================================
+#Error handler for abort(404) 
+@app.errorhandler(404)
+def not_found(error):
+    #return make_response(jsonify({'error': 'Not found'}), 404)
+	response = jsonify({'result': 'Failed', 'message':  error.description['message']})
+	response.status_code = 404
+	return response
+
+#Error handler for abort(400) 
+@app.errorhandler(400)
+def type_error(error):
+    #return make_response(jsonify({'error': 'type error'}), 400)
+    response = jsonify({'result': 'Failed', 'message':  error.description['message']})
+    response.status_code = 400
+    return response
+	
+#Error handler for abort(401) 
+@app.errorhandler(401)
+def access_deny(error):
+    response = jsonify({'result': 'Failed', 'message':  error.description['message']})
+    response.status_code = 401
+    return response
+
+	
+#========================================== Request handler ===============================================	
+#GET query indexToken for specific index_id
+@app.route('/indexauth/api/v1.0/getIndexToken', methods=['GET'])
+def getIndexToken():
+	#print request.data
+	index_id = request.args.get('index_id', type = str)
+	json_data = IndexPolicy.get_indexToken(index_id)
+	return jsonify({'result': 'Succeed', 'data': json_data}), 201
+
+#GET query authorized nodes
+@app.route('/indexauth/api/v1.0/getAuthorizedNodes', methods=['GET'])
+def getAuthorizedNodes():
+	#print request.data
+	json_data = IndexPolicy.get_AuthorizedNodes()
+	return jsonify({'result': 'Succeed', 'data': json_data}), 201
+
+#GET apply for verify_indexToken service for specific index_id
+@app.route('/indexauth/api/v1.0/verify_indexToken', methods=['GET'])
+def verify_indexToken():
+	#print request.data
+	index_id = request.args.get('index_id', type = str)
+	filepath = request.args.get('index_data', type = str)
+	json_data = IndexPolicy.verify_indexToken(index_id,filepath)
+	return jsonify({'result': 'Succeed', 'data': json_data}), 201
+
+	
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
+
+
+
+
---
+++ Services_dev/IndexAuth/app/src/Test_Client.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3.5
+
+'''
+========================
+Test_Client module
+========================
+Created on Dec.27, 2018
+@author: Xu Ronghua
+@Email:  rxu22@binghamton.edu
+@TaskDescription: This module provide encapsulation of client API that access to Web service.
+'''
+import time
+import requests
+import datetime
+import json
+
+#from CapAC_Policy import CapToken
+
+import sys
+from utilities import DatetimeUtil, TypesUtil, FileUtil
+from wrapper_pyca import Crypto_DSA
+
+now = datetime.datetime.now()
+datestr=now.strftime("%Y-%m-%d")
+timestr=now.strftime("%H:%M:%S")
+    
+class WSClient(object):
+    '''
+    Get IndexAuth_Token
+    @host_addr: ip_address:port_num
+    '''
+    @staticmethod
+    def getIndexToken(host_addr, index_id, data_args={}):
+        #construct api_url
+        api_url = "http://" + host_addr + "/indexauth/api/v1.0/getIndexToken"
+        params={}
+        params['index_id']=index_id
+        headers = {'Content-Type' : 'application/json'}
+        response = requests.get(api_url,params=params, data=json.dumps(data_args), headers=headers)
+
+        #get response json
+        json_response = response.json()
+
+        return json_response
+
+    '''
+    Get IndexAuth_Token
+    @host_addr: ip_address:port_num
+    '''
+    @staticmethod
+    def getAuthorizedNodes(host_addr, data_args={}):
+        #construct api_url
+        api_url = "http://" + host_addr + "/indexauth/api/v1.0/getAuthorizedNodes"
+        headers = {'Content-Type' : 'application/json'}
+        response = requests.get(api_url,data=json.dumps(data_args), headers=headers)
+
+        #get response json
+        json_response = response.json()
+
+        return json_response
+
+    '''
+    Get IndexAuth_Token
+    @host_addr: ip_address:port_num
+    '''
+    @staticmethod
+    def verify_indexToken(host_addr, index_id, index_data, data_args={}):
+        #construct api_url
+        api_url = "http://" + host_addr + "/indexauth/api/v1.0/verify_indexToken"
+        params={}
+        params['index_id']=index_id
+        params['index_data']=index_data
+        headers = {'Content-Type' : 'application/json'}
+        response = requests.get(api_url,params=params, data=json.dumps(data_args), headers=headers)
+
+        #get response json
+        json_response = response.json()
+
+        return json_response
+
+def test_func():
+	filepath = './features/0_2_person1/13.2.52.txt'
+	filepath0 = './features/0_2_person1/13.4.53.txt'
+
+	index_id = '1'
+	start_time=time.time()
+
+	print(WSClient.getIndexToken('128.226.77.237:8080', index_id))
+	print(WSClient.getAuthorizedNodes('128.226.77.237:8080'))	
+	print(WSClient.verify_indexToken('128.226.77.237:8080', index_id, filepath))
+	
+	end_time=time.time()
+
+        #calculate exec time
+	exec_time=end_time-start_time
+	
+	time_exec=format(exec_time*1000, '.3f')
+	print("Execution time is:%2.6f" %(exec_time))
+	
+	FileUtil.AddLine('exec_time_client.log', time_exec)
+  
+
+	
+if __name__ == "__main__":
+	test_func()
+	pass
