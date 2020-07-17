89a0c26cf6fd0458923d4ad95aa87d0ad0e3be74
samuelxu999 <samuelxu999@gmail.com>
2020-07-16 23:13:39 -0400

    enable threaded for server.py

--- Services_dev/BlendCAC/app/src/BlendCAC_server.py
+++ Services_dev/BlendCAC/app/src/BlendCAC_server.py
@@ -66,7 +66,7 @@ def isValidAccess():
 
 	
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
 
 
 
--- Services_dev/IndexAuth/app/src/IndexAuth_Server.py
+++ Services_dev/IndexAuth/app/src/IndexAuth_Server.py
@@ -74,7 +74,7 @@ def verify_indexToken():
 
 	
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
 
 
 
