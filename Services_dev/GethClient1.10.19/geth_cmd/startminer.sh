72050d5c99693c46690b9d3f1b5c90a245d0d11e
samuelxu999 <samuelxu999@gmail.com>
2022-06-17 09:04:00 -0400

    Update geth command and genesis.json for GethClient1.10.19.

--- Services_dev/GethClient1.10.19/geth_cmd/startminer.sh
+++ Services_dev/GethClient1.10.19/geth_cmd/startminer.sh
@@ -18,7 +18,20 @@ fi
 
 ## copy static-nodes.json to account folder
 /bin/cp /home/docker/node_data/static-nodes.json /home/docker/account/
-#--unlock 0 --password ${HOME}/account/password.sec
+
 
 ## launch geth client app
-/opt/go-ethereum/build/bin/geth --syncmode full --identity "geth_node" --networkid 42 --datadir "/home/docker/account" --nodiscover --mine --miner.threads=1 --http --http.port "8042" --port "30303" --ipcpath "/home/docker/.ethereum/geth.ipc"
+/opt/go-ethereum/build/bin/geth \
+--identity "geth_node" \
+--networkid 2104 \
+--datadir "/home/docker/account" \
+--nodiscover \
+--syncmode full \
+--mine \
+--miner.threads=1 \
+--http --http.port "8042" \
+--port "30303" \
+--allow-insecure-unlock \
+--unlock 0 \
+--password /home/docker/account/password.sec \
+--ipcpath "/home/docker/.ethereum/geth.ipc"
